2cd75e0f9f4fb884e8637c805525a89210a34443
Vlad <vladvoytkiv321@gmail.com>
2024-12-23 20:04:50 +0200

    Add AsyncEventEmitter class with subscription functionality
    This commit introduces the AsyncEventEmitter class, enabling reactive, message-based communication between entities. It includes methods for subscribing callbacks to specific events using an asyncio-based implementation.

--- tasks/Task 5/task_5.py
+++ tasks/Task 5/task_5.py
@@ -1,1 +1,11 @@
 # Task 5 (Observable/EventEmitter/Alternative) -- Reactive message based communication between entities
+import asyncio
+from collections import defaultdict
+
+class AsyncEventEmitter:
+    def __init__(self):
+        self.events = defaultdict(list)
+
+    async def subscribe(self, event_name, callback):
+        if callback not in self.events[event_name]:
+            self.events[event_name].append(callback)
