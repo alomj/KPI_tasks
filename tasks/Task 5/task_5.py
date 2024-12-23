84e87ecc1bdb88740a08171424296b5cf3016275
Vlad <vladvoytkiv321@gmail.com>
2024-12-23 20:08:23 +0200

    Add unsubscribe and emit methods to event system
    Introduced `unsubscribe` to remove callbacks and clean up unused events. Added `emit` to trigger all callbacks for a given event asynchronously. These changes enhance the flexibility and functionality of the event handling system.

--- tasks/Task 5/task_5.py
+++ tasks/Task 5/task_5.py
@@ -9,3 +9,16 @@ class AsyncEventEmitter:
     async def subscribe(self, event_name, callback):
         if callback not in self.events[event_name]:
             self.events[event_name].append(callback)
+
+    async def unsubscribe(self, event_name, callback):
+        if callback in self.events[event_name]:
+            self.events[event_name].remove(callback)
+            if not self.events[event_name]:
+                del self.events[event_name]
+
+    async def emit(self, event_name, *args, **kwargs):
+        if event_name in self.events:
+            await asyncio.gather(
+                *(callback(*args, **kwargs) for callback in self.events[event_name])
+            )
+
