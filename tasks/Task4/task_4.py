0902d7975fa7a5a1457ab045ece5bfb2040e7cc6
Vlad <vladvoytkiv321@gmail.com>
2024-12-23 18:57:59 +0200

    Refactor task to include main entry point and improve logging.
    Added a `main` function to improve clarity and usability of the script. Adjusted chunk size to 25 for better performance and added logging to display processed text and file being read.

--- tasks/Task4/task_4.py
+++ tasks/Task4/task_4.py
@@ -2,11 +2,24 @@
 import asyncio
 import aiofiles
 
-input_file_path = "text.txt"
 
-async def async_text_processor(file_path, chunk_size=10):
+
+async def async_text_processor(file_path, chunk_size=25):
 
     async with aiofiles.open(file_path, 'r', encoding='utf-8') as file:
         while chunk := await file.read(chunk_size):
             await asyncio.sleep(0.1)
             yield chunk.upper()
+
+async def main():
+    input_file_path = "text.txt"
+
+    print(f"Read and processing file: {input_file_path}")
+    print("\n Processed text (chunk by chunk):")
+
+    async for processed_chunk in async_text_processor(input_file_path, chunk_size=25):
+        print(f"[CHUNK]: {processed_chunk}")
+
+if __name__ == "__main__":
+    import aiofiles
+    asyncio.run(main())
