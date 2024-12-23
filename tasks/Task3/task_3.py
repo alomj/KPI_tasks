296d3eb5b203b548e877932d0e0ad77739711513
Vlad <vladvoytkiv321@gmail.com>
2024-12-23 17:01:40 +0200

    initialisation class AbortController

--- tasks/Task3/task_3.py
+++ tasks/Task3/task_3.py
@@ -2,3 +2,23 @@
 #   * Integrate AbortController or other Cancallable approach
 
 
+import asyncio
+
+class AbortController:
+    def __init__(self):
+        self._cancel_event = asyncio.Event()
+        self._cancelled = False
+
+    async def abort(self):
+        if not self._cancelled:
+            self._cancelled = True
+            self._cancel_event.set()
+
+    async def wait_for_abort(self):
+        await self._cancel_event.wait()
+
+    def is_aborted(self):
+        return self._cancelled
+
+
+
