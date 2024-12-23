c2fd379b74ca5252fcaed6385ca901040abe3e41
Vlad <vladvoytkiv321@gmail.com>
2024-12-23 18:13:22 +0200

    Add async text processor for streaming large files
    Introduced `async_text_processor` function to read and process large files asynchronously in chunks. This implementation uses `aiofiles` for non-blocking file I/O and yields uppercase chunks to support efficient and scalable data handling.

--- tasks/Task4/task_4.py
+++ tasks/Task4/task_4.py
@@ -1,1 +1,12 @@
 # Task 4 (Stream/AsyncIterator/Alternative) -- Ongoing processing of large data sets that do not fit in memory
+import asyncio
+import aiofiles
+
+input_file_path = "text.txt"
+
+async def async_text_processor(file_path, chunk_size=10):
+
+    async with aiofiles.open(file_path, 'r', encoding='utf-8') as file:
+        while chunk := await file.read(chunk_size):
+            await asyncio.sleep(0.1)
+            yield chunk.upper()
